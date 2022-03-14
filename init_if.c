59f5ca66438de251f7591b24f6dc4b23598dd88b
ajarmusch <jarmusch@udel.edu>
2022-03-14 15:14:53 -0400

    init tests

---
+++ init_if.c
@@ -0,0 +1,87 @@
+#include "acc_testsuite.h"
+#ifndef T1
+//T1:init,runtime,V:3.0-3.2
+int test1(){
+    int err = 0;
+    srand(SEED);
+    real_t * a = (real_t *)malloc(n * sizeof(real_t));
+    real_t * b = (real_t *)malloc(n * sizeof(real_t));
+
+    for (int x = 0; x < n; ++x){
+        a[x] = rand() / (real_t)(RAND_MAX / 10);
+        b[x] = 0;
+    }
+
+    for (int x = 0; x < n; ++x){
+        #pragma acc init if(a[x] = a[x])
+        {
+            b[x] = a[x];
+        }
+    }
+
+    for (int x = 0; x < n; ++x){
+        if (fabs(a[x] - b[x]) > PRECISION){
+            err += 1;
+        }
+    }
+
+    return err;
+}
+#endif
+
+#ifndef T2
+//T2:init,runtime,neg,V:3.0-3.2
+int test2(){
+    int err = 0;
+    srand(SEED);
+    real_t * a = (real_t *)malloc(n * sizeof(real_t));
+    real_t * b = (real_t *)malloc(n * sizeof(real_t));
+    real_t * c = (real_t *)malloc(n * sizeof(real_t));
+
+    for (int x = 0; x < n; ++x){
+        a[x] = rand() / (real_t)(RAND_MAX / 10);
+        b[x] = rand() / (real_t)(RAND_MAX / 10);
+        c[x] = a[x];
+    }
+
+    for (int x = 0; x < n; ++x){
+        #pragma acc init if(a[x] = b[x])
+        {
+            c[x] = a[x] * 2;
+        }
+
+    }
+
+    for (int x = 0; x < n; ++x){
+        if (fabs(a[x] - c[x]) > PRECISION){
+            err += 1;
+        }
+    }
+
+    return err;
+}
+#endif
+
+int main(){
+    int failcode = 0;
+    int failed;
+#ifndef T1
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test1();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 0);
+    }
+#endif
+#ifndef T2
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test2();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 1);
+    }
+#endif
+    return failcode;
+}
